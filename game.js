215e3dcd5849a2e7ab69885835eba013afc7d836
Nestor Garcia <nestorjgcc@hotmail.com>
2022-05-11 20:31:48 -0700

    Trigger rebuild

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+let gamePattern = [];
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-  console.log(randomNumber);
-
+  /* console.log(randomNumber); */
   let randomChosenColour = buttonColours[randomNumber];
   console.log(randomChosenColour);
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
